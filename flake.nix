edd757f74862a259e43111e19c1e19b871a97c8a
Warren Hood <nullbyte001@gmail.com>
2024-07-22 23:53:19 +0200

    Upgrade home manager release

--- flake.nix
+++ flake.nix
@@ -4,14 +4,16 @@
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
     # nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager,... }@inputs: 
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager,... }@inputs: 
   let
     system = "x86_64-linux";
     user = "warren";
