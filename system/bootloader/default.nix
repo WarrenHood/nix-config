89e435361cea012ff5368ae7668e3f0da8bfc995
Warren Hood <nullbyte001@gmail.com>
2024-09-21 16:03:55 +0200

    Enable NTFS filesystem support

--- system/bootloader/default.nix
+++ system/bootloader/default.nix
@@ -2,4 +2,7 @@
   # We'll use systemd-boot for now...
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # Enable NTFS at boot
+  boot.supportedFilesystems = [ "ntfs" ];
 }
