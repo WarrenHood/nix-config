776822488cdcf6058df6810721695d9aa5e7ee7e
Warren Hood <nullbyte001@gmail.com>
2024-09-21 14:15:06 +0200

    Added sleepy launcher

--- system/optional/animegames.nix
+++ system/optional/animegames.nix
@@ -2,4 +2,5 @@ aagl: { pkgs, ... }: {
   nix.settings = aagl.nixConfig; # Cachix
   # programs.anime-game-launcher.enable = true;
   programs.honkers-railway-launcher.enable = true;
+  programs.sleepy-launcher.enable = true;
 }
