f660f537875fab48f1cb671099bb0557329b2509
Warren Hood <nullbyte001@gmail.com>
2024-09-29 00:13:26 +0200

    Enable gamescope for star citizen and move location to games partition

--- home/star-citizen.nix
+++ home/star-citizen.nix
@@ -2,6 +2,8 @@
   home.packages = [
     (inputs.nix-gaming.packages.${pkgs.hostPlatform.system}.star-citizen.override {
       tricks = [ "arial" "vcrun2019" "win10" "sound=alsa" ];
+      gameScopeEnable = true;
+      location = "/games/star-citizen";
     })
   ];
 }
