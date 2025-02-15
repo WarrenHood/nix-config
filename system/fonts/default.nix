de008c6469b492282f2756afee7f51cfd631cc84
Warren Hood <nullbyte001@gmail.com>
2025-02-15 18:03:55 +0200

    Added noto fonts

--- system/fonts/default.nix
+++ system/fonts/default.nix
@@ -4,6 +4,9 @@
     # Stable NixOS 24.11 nerd fonts
     (nerdfonts.override { fonts = [ "Mononoki" "JetBrainsMono" ]; })
 
+    # Might as well have noto fonts
+    noto-fonts
+
     # Nerd fonts for nixos-unstable
     # nerd-fonts.mononoki nerd-fonts.jetbrains-mono
   ];
