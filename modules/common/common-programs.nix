9700a183541f6c61c3411ef59d6b224fd5b1b304
Warren Hood <nullbyte001@gmail.com>
2026-01-30 02:06:18 +0000

    add common programs

---
+++ modules/common/common-programs.nix
@@ -0,0 +1,28 @@
+{inputs, ...}: {
+  flake.nixosModules.commonPrograms = {pkgs, ...}: {
+    # nix-ld to allow VS Code Remote to work for example
+    programs.nix-ld.enable = true;
+    programs.nix-ld.libraries = with pkgs; [
+      # Add any missing dynamic libraries for unpackaged programs
+      # here, NOT in environment.systemPackages
+    ];
+
+    # Some basic packages that I will almost always use
+    environment.systemPackages = with pkgs; [
+      vim # In case neovim ever breaks
+      clang # For tree-sitter in Neovim
+      wget
+      git
+      htop
+      ncdu
+      stow
+      gnumake
+      neofetch
+      fd
+      ripgrep
+      lshw
+      p7zip
+      tmux
+    ];
+  };
+}
--- modules/hosts/g14-wsl/nixos.nix
+++ modules/hosts/g14-wsl/nixos.nix
@@ -3,6 +3,7 @@
     system = "x86_64-linux";
     modules = [
       inputs.self.nixosModules.commonSystemConfig # Common system configs
+      inputs.self.nixosModules.commonPrograms # Install common programs
       inputs.self.nixosModules.g14-wsl # Host-specific config
       inputs.self.nixosModules.warren # Warren user
       inputs.self.nixosModules.useZSH # Use the default shell
