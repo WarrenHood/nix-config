c21036a3d12b301f915ab0dc60738e37f2e8920c
Warren Hood <nullbyte001@gmail.com>
2026-02-04 20:15:10 +0000

    use a single nix file for the g14-wsl host and add update hostname

--- modules/hosts/g14-wsl.nix
+++ modules/hosts/g14-wsl.nix
@@ -4,9 +4,19 @@
     modules = [
       inputs.self.modules.nixos.commonSystemConfig # Common system configs
       inputs.self.modules.nixos.commonPrograms # Install common programs
-      inputs.self.modules.nixos.g14-wsl # Host-specific config
       inputs.self.modules.nixos.warren # Warren user
       inputs.self.modules.nixos.useZSH # Use the default shell
+      # Host specific config
+      {
+        imports = [
+          # include NixOS-WSL modules
+          <nixos-wsl/modules>
+        ];
+
+        networking.hostName = "g14-wsl";
+        wsl.enable = true;
+        wsl.defaultUser = "warren";
+      }
     ];
   };
 }
