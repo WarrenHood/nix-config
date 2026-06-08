e945490c100227891acb3fe89e39e9c43bc50967
Warren Hood <nullbyte001@gmail.com>
2026-06-08 23:01:23 +0200

    auto optimise nix store and enable auto gc weekly

--- modules/nixos-modules/system-config.nix
+++ modules/nixos-modules/system-config.nix
@@ -28,6 +28,15 @@
       # Allow unfree packages
       nixpkgs.config.allowUnfree = true;
 
+      # Auto-optimise the store
+      nix.settings.auto-optimise-store = true;
+
+      nix.gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 30d";
+      };
+
       # This value determines the NixOS release from which the default
       # settings for stateful data, like file locations and database versions
       # on your system were taken. It‘s perfectly fine and recommended to leave
