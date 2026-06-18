55b875682f7065037cb9ac698fbfb07b293d0531
Warren Hood <nullbyte001@gmail.com>
2026-06-18 21:19:58 +0200

    Add Kanshi profile for new monitor

--- modules/wayland/displays.nix
+++ modules/wayland/displays.nix
@@ -23,6 +23,26 @@
             }
           ];
         }
+
+        {
+          profile.name = "home-g14-with-alienware";
+          profile.outputs = [
+            # rog-g14 display
+            {
+              criteria = "Thermotrex Corporation TL140ADXP02-0 Unknown";
+              mode = "2560x1600@165.002";
+              position = "0,0";
+              scale = 1.5;
+            }
+
+            # External monitor - Alienware 1440p
+            {
+              criteria = "Dell Inc. AW2725DF JRS7ZZ3";
+              mode = "2560x1440@144.00";
+              position = "1707,0";
+            }
+          ];
+        }
       ];
     };
   };
