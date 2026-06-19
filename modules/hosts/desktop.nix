5b7c7d57b5c46459a611bf1a8149aff1732ec4db
Warren Hood <nullbyte001@gmail.com>
2026-06-19 20:36:48 +0200

    add osu lazer to desktop

--- modules/hosts/desktop.nix
+++ modules/hosts/desktop.nix
@@ -31,6 +31,13 @@
             gamingBase
             # niriConfig
             # noctaliaShell
+
+            ({pkgs, ...}: {
+              home.packages = with pkgs; [
+                osu-lazer
+              ];
+            })
+
           ];
         };
       }
