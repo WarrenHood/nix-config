984793ca86da01b6127d4f3f1923c3ea10c0bb57
Warren Hood <nullbyte001@gmail.com>
2026-07-28 23:30:31 +0200

    set tcp_slow_start_after_idle=0

--- modules/nixos-bases/gaming.nix
+++ modules/nixos-bases/gaming.nix
@@ -81,6 +81,7 @@
     boot.kernel.sysctl = {
       "net.core.default_qdisc" = "fq";
       "net.ipv4.tcp_congestion_control" = "bbr";
+      "net.ipv4.tcp_slow_start_after_idle" = 0;
     };
   };
 }
