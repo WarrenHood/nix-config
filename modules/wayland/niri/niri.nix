22fbb15f3d153b47527a2dd85418502a03462794
Warren Hood <nullbyte001@gmail.com>
2026-08-14 20:21:54 +0200

    switch back to the nixpkgs niri until the sodiboo flake is fixed

--- modules/wayland/niri/niri.nix
+++ modules/wayland/niri/niri.nix
@@ -1,43 +1,47 @@
-{
-  inputs,
-  self,
-  ...
+{ inputs
+, self
+, ...
 }: {
-  flake.modules.nixos.niri = {
-    lib,
-    pkgs,
-    ...
-  }: {
-    imports = with self.modules.nixos; [
-      waylandBase
-      inputs.niri.nixosModules.niri
-    ];
+  flake.modules.nixos.niri =
+    { lib
+    , pkgs
+    , ...
+    }: {
+      imports = with self.modules.nixos; [
+        waylandBase
+        inputs.niri.nixosModules.niri
+      ];
 
-    # Use niri-unstable since niri-stable is kinda behind
-    programs.niri.package = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri-unstable;
+      # Use niri-unstable since niri-stable is kinda behind
+      # programs.niri.package = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri-unstable;
 
-    programs.niri.enable = true;
+      # Actually, niri-unstable is kinda broken because libdisplay-info 0.2 was removed in the latest unstable nixpkgs
+      # I'll maybe switch back to the sodiboo niri flake package once https://github.com/sodiboo/niri-flake/pull/1853 is merged in
+      programs.niri.package = pkgs.niri;
 
-    xdg.portal.enable = true;
-    xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gnome];
-    xdg.portal.config = {
-      common = {
-        default = lib.mkForce ["gnome"];
+      programs.niri.enable = true;
+
+      xdg.portal.enable = true;
+      xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
+      xdg.portal.config = {
+        common = {
+          default = lib.mkForce [ "gnome" ];
+        };
       };
+
+      # TODO: Is xwayland-satellite included by default when niri is enabled?
+      environment.systemPackages = with pkgs; [
+        xwayland-satellite
+      ];
     };
 
-    # TODO: Is xwayland-satellite included by default when niri is enabled?
-    environment.systemPackages = with pkgs; [
-      xwayland-satellite
-    ];
-  };
-
-  flake.modules.homeManager.niriConfig = {pkgs, ...}: {
+  flake.modules.homeManager.niriConfig = { pkgs, ... }: {
     imports = with self.modules.homeManager; [
       waylandBase
     ];
 
-    programs.niri.package = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri-unstable;
+    # programs.niri.package = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri-unstable;
+    programs.niri.package = pkgs.niri;
 
     programs.niri.settings = {
       # Prefer no client side decorations
@@ -45,7 +49,7 @@
       # Keybinds
       binds = {
         # shows a list of important hotkeys.
-        "Mod+Shift+Slash".action.show-hotkey-overlay = {};
+        "Mod+Shift+Slash".action.show-hotkey-overlay = { };
 
         # Open alacritty
         "Mod+Return".action.spawn = "alacritty";
@@ -62,124 +66,124 @@
         # Volume controls for pipewire
         "XF86AudioRaiseVolume" = {
           allow-when-locked = true;
-          action.spawn = ["wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+"];
+          action.spawn = [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+" ];
         };
         "XF86AudioLowerVolume" = {
           allow-when-locked = true;
-          action.spawn = ["wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-"];
+          action.spawn = [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-" ];
         };
         "XF86AudioMute" = {
           allow-when-locked = true;
-          action.spawn = ["wpctl" "set-mute" "@DEFAULT_AUDIO_SINK@" "toggle"];
+          action.spawn = [ "wpctl" "set-mute" "@DEFAULT_AUDIO_SINK@" "toggle" ];
         };
         "XF86AudioMicMute" = {
           allow-when-locked = true;
-          action.spawn = ["wpctl" "set-mute" "@DEFAULT_AUDIO_SOURCE@" "toggle"];
+          action.spawn = [ "wpctl" "set-mute" "@DEFAULT_AUDIO_SOURCE@" "toggle" ];
         };
 
         # Brightness control
         "XF86MonBrightnessUp" = {
           allow-when-locked = true;
-          action.spawn = ["brightnessctl" "--class=backlight" "set" "+10%"];
+          action.spawn = [ "brightnessctl" "--class=backlight" "set" "+10%" ];
         };
         "XF86MonBrightnessDown" = {
           allow-when-locked = true;
-          action.spawn = ["brightnessctl" "--class=backlight" "set" "10%-"];
+          action.spawn = [ "brightnessctl" "--class=backlight" "set" "10%-" ];
         };
 
         # Toggle overview
         "Mod+O" = {
           repeat = false;
-          action.toggle-overview = {};
+          action.toggle-overview = { };
         };
 
         # Close a window
         "Mod+Shift+C" = {
           repeat = false;
-          action.close-window = {};
+          action.close-window = { };
         };
 
-        "Mod+Left".action.focus-column-left = {};
-        "Mod+Down".action.focus-window-down = {};
-        "Mod+Up".action.focus-window-up = {};
-        "Mod+Right".action.focus-column-right = {};
-        "Mod+H".action.focus-column-left = {};
-        "Mod+J".action.focus-window-down = {};
-        "Mod+K".action.focus-window-up = {};
-        "Mod+L".action.focus-column-right = {};
+        "Mod+Left".action.focus-column-left = { };
+        "Mod+Down".action.focus-window-down = { };
+        "Mod+Up".action.focus-window-up = { };
+        "Mod+Right".action.focus-column-right = { };
+        "Mod+H".action.focus-column-left = { };
+        "Mod+J".action.focus-window-down = { };
+        "Mod+K".action.focus-window-up = { };
+        "Mod+L".action.focus-column-right = { };
 
-        "Mod+Ctrl+Left".action.move-column-left = {};
-        "Mod+Ctrl+Down".action.move-window-down = {};
-        "Mod+Ctrl+Up".action.move-window-up = {};
-        "Mod+Ctrl+Right".action.move-column-right = {};
-        "Mod+Ctrl+H".action.move-column-left = {};
-        "Mod+Ctrl+J".action.move-window-down = {};
-        "Mod+Ctrl+K".action.move-window-up = {};
-        "Mod+Ctrl+L".action.move-column-right = {};
+        "Mod+Ctrl+Left".action.move-column-left = { };
+        "Mod+Ctrl+Down".action.move-window-down = { };
+        "Mod+Ctrl+Up".action.move-window-up = { };
+        "Mod+Ctrl+Right".action.move-column-right = { };
+        "Mod+Ctrl+H".action.move-column-left = { };
+        "Mod+Ctrl+J".action.move-window-down = { };
+        "Mod+Ctrl+K".action.move-window-up = { };
+        "Mod+Ctrl+L".action.move-column-right = { };
 
-        "Mod+Home".action.focus-column-first = {};
-        "Mod+End".action.focus-column-last = {};
-        "Mod+Ctrl+Home".action.move-column-to-first = {};
-        "Mod+Ctrl+End".action.move-column-to-last = {};
+        "Mod+Home".action.focus-column-first = { };
+        "Mod+End".action.focus-column-last = { };
+        "Mod+Ctrl+Home".action.move-column-to-first = { };
+        "Mod+Ctrl+End".action.move-column-to-last = { };
 
-        "Mod+Shift+Left".action.focus-monitor-left = {};
-        "Mod+Shift+Down".action.focus-monitor-down = {};
-        "Mod+Shift+Up".action.focus-monitor-up = {};
-        "Mod+Shift+Right".action.focus-monitor-right = {};
-        "Mod+Shift+H".action.focus-monitor-left = {};
-        "Mod+Shift+J".action.focus-monitor-down = {};
-        "Mod+Shift+K".action.focus-monitor-up = {};
-        "Mod+Shift+L".action.focus-monitor-right = {};
+        "Mod+Shift+Left".action.focus-monitor-left = { };
+        "Mod+Shift+Down".action.focus-monitor-down = { };
+        "Mod+Shift+Up".action.focus-monitor-up = { };
+        "Mod+Shift+Right".action.focus-monitor-right = { };
+        "Mod+Shift+H".action.focus-monitor-left = { };
+        "Mod+Shift+J".action.focus-monitor-down = { };
+        "Mod+Shift+K".action.focus-monitor-up = { };
+        "Mod+Shift+L".action.focus-monitor-right = { };
 
-        "Mod+Shift+Ctrl+Left".action.move-column-to-monitor-left = {};
-        "Mod+Shift+Ctrl+Down".action.move-column-to-monitor-down = {};
-        "Mod+Shift+Ctrl+Up".action.move-column-to-monitor-up = {};
-        "Mod+Shift+Ctrl+Right".action.move-column-to-monitor-right = {};
-        "Mod+Shift+Ctrl+H".action.move-column-to-monitor-left = {};
-        "Mod+Shift+Ctrl+J".action.move-column-to-monitor-down = {};
-        "Mod+Shift+Ctrl+K".action.move-column-to-monitor-up = {};
-        "Mod+Shift+Ctrl+L".action.move-column-to-monitor-right = {};
+        "Mod+Shift+Ctrl+Left".action.move-column-to-monitor-left = { };
+        "Mod+Shift+Ctrl+Down".action.move-column-to-monitor-down = { };
+        "Mod+Shift+Ctrl+Up".action.move-column-to-monitor-up = { };
+        "Mod+Shift+Ctrl+Right".action.move-column-to-monitor-right = { };
+        "Mod+Shift+Ctrl+H".action.move-column-to-monitor-left = { };
+        "Mod+Shift+Ctrl+J".action.move-column-to-monitor-down = { };
+        "Mod+Shift+Ctrl+K".action.move-column-to-monitor-up = { };
+        "Mod+Shift+Ctrl+L".action.move-column-to-monitor-right = { };
 
-        "Mod+Page_Down".action.focus-workspace-down = {};
-        "Mod+Page_Up".action.focus-workspace-up = {};
-        "Mod+U".action.focus-workspace-down = {};
-        "Mod+I".action.focus-workspace-up = {};
-        "Mod+Ctrl+Page_Down".action.move-column-to-workspace-down = {};
-        "Mod+Ctrl+Page_Up".action.move-column-to-workspace-up = {};
-        "Mod+Ctrl+U".action.move-column-to-workspace-down = {};
-        "Mod+Ctrl+I".action.move-column-to-workspace-up = {};
+        "Mod+Page_Down".action.focus-workspace-down = { };
+        "Mod+Page_Up".action.focus-workspace-up = { };
+        "Mod+U".action.focus-workspace-down = { };
+        "Mod+I".action.focus-workspace-up = { };
+        "Mod+Ctrl+Page_Down".action.move-column-to-workspace-down = { };
+        "Mod+Ctrl+Page_Up".action.move-column-to-workspace-up = { };
+        "Mod+Ctrl+U".action.move-column-to-workspace-down = { };
+        "Mod+Ctrl+I".action.move-column-to-workspace-up = { };
 
-        "Mod+Shift+Page_Down".action.move-workspace-down = {};
-        "Mod+Shift+Page_Up".action.move-workspace-up = {};
-        "Mod+Shift+U".action.move-workspace-down = {};
-        "Mod+Shift+I".action.move-workspace-up = {};
+        "Mod+Shift+Page_Down".action.move-workspace-down = { };
+        "Mod+Shift+Page_Up".action.move-workspace-up = { };
+        "Mod+Shift+U".action.move-workspace-down = { };
+        "Mod+Shift+I".action.move-workspace-up = { };
 
         "Mod+WheelScrollDown" = {
           cooldown-ms = 150;
-          action.focus-workspace-down = {};
+          action.focus-workspace-down = { };
         };
         "Mod+WheelScrollUp" = {
           cooldown-ms = 150;
-          action.focus-workspace-up = {};
+          action.focus-workspace-up = { };
         };
         "Mod+Ctrl+WheelScrollDown" = {
           cooldown-ms = 150;
-          action.move-column-to-workspace-down = {};
+          action.move-column-to-workspace-down = { };
         };
         "Mod+Ctrl+WheelScrollUp" = {
           cooldown-ms = 150;
-          action.move-column-to-workspace-up = {};
+          action.move-column-to-workspace-up = { };
         };
 
-        "Mod+WheelScrollRight".action.focus-column-right = {};
-        "Mod+WheelScrollLeft".action.focus-column-left = {};
-        "Mod+Ctrl+WheelScrollRight".action.move-column-right = {};
-        "Mod+Ctrl+WheelScrollLeft".action.move-column-left = {};
+        "Mod+WheelScrollRight".action.focus-column-right = { };
+        "Mod+WheelScrollLeft".action.focus-column-left = { };
+        "Mod+Ctrl+WheelScrollRight".action.move-column-right = { };
+        "Mod+Ctrl+WheelScrollLeft".action.move-column-left = { };
 
-        "Mod+Shift+WheelScrollDown".action.focus-column-right = {};
-        "Mod+Shift+WheelScrollUp".action.focus-column-left = {};
-        "Mod+Ctrl+Shift+WheelScrollDown".action.move-column-right = {};
-        "Mod+Ctrl+Shift+WheelScrollUp".action.move-column-left = {};
+        "Mod+Shift+WheelScrollDown".action.focus-column-right = { };
+        "Mod+Shift+WheelScrollUp".action.focus-column-left = { };
+        "Mod+Ctrl+Shift+WheelScrollDown".action.move-column-right = { };
+        "Mod+Ctrl+Shift+WheelScrollUp".action.move-column-left = { };
 
         "Mod+1".action.focus-workspace = 1;
         "Mod+2".action.focus-workspace = 2;
@@ -200,36 +204,36 @@
         "Mod+Ctrl+8".action.move-column-to-workspace = 8;
         "Mod+Ctrl+9".action.move-column-to-workspace = 9;
 
-        "Mod+BracketLeft".action.consume-or-expel-window-left = {};
-        "Mod+BracketRight".action.consume-or-expel-window-right = {};
+        "Mod+BracketLeft".action.consume-or-expel-window-left = { };
+        "Mod+BracketRight".action.consume-or-expel-window-right = { };
 
-        "Mod+Comma".action.consume-window-into-column = {};
-        "Mod+Period".action.expel-window-from-column = {};
+        "Mod+Comma".action.consume-window-into-column = { };
+        "Mod+Period".action.expel-window-from-column = { };
 
-        "Mod+R".action.switch-preset-column-width = {};
-        "Mod+Shift+R".action.switch-preset-window-height = {};
-        "Mod+Ctrl+R".action.reset-window-height = {};
-        "Mod+F".action.maximize-column = {};
-        "Mod+Shift+F".action.fullscreen-window = {};
+        "Mod+R".action.switch-preset-column-width = { };
+        "Mod+Shift+R".action.switch-preset-window-height = { };
+        "Mod+Ctrl+R".action.reset-window-height = { };
+        "Mod+F".action.maximize-column = { };
+        "Mod+Shift+F".action.fullscreen-window = { };
 
-        "Mod+Ctrl+F".action.expand-column-to-available-width = {};
-        "Mod+C".action.center-column = {};
+        "Mod+Ctrl+F".action.expand-column-to-available-width = { };
+        "Mod+C".action.center-column = { };
 
-        "Mod+Ctrl+C".action.center-visible-columns = {};
+        "Mod+Ctrl+C".action.center-visible-columns = { };
 
         "Mod+Minus".action.set-column-width = "-10%";
         "Mod+Equal".action.set-column-width = "+10%";
         "Mod+Shift+Minus".action.set-window-height = "-10%";
         "Mod+Shift+Equal".action.set-window-height = "+10%";
 
-        "Mod+V".action.toggle-window-floating = {};
-        "Mod+Shift+V".action.switch-focus-between-floating-and-tiling = {};
+        "Mod+V".action.toggle-window-floating = { };
+        "Mod+Shift+V".action.switch-focus-between-floating-and-tiling = { };
 
-        "Mod+W".action.toggle-column-tabbed-display = {};
+        "Mod+W".action.toggle-column-tabbed-display = { };
 
-        "Print".action.screenshot = {};
-        "Ctrl+Print".action.screenshot-screen = {};
-        "Alt+Print".action = {"screenshot-window" = {};};
+        "Print".action.screenshot = { };
+        "Ctrl+Print".action.screenshot-screen = { };
+        "Alt+Print".action = { "screenshot-window" = { }; };
 
         # Screenshot region with annotation using grim + slurp + satty
         "Mod+Shift+S".action.spawn-sh = ''grim -g "$(slurp)" - | satty --filename - --copy-command "wl-copy"'';
@@ -237,11 +241,11 @@
         # An escape hatch in case something like a remote desktop window or VM windows doesn't want to give up focus
         "Mod+Escape" = {
           allow-inhibiting = false;
-          action.toggle-keyboard-shortcuts-inhibit = {};
+          action.toggle-keyboard-shortcuts-inhibit = { };
         };
 
-        "Mod+Shift+E".action.quit = {};
-        "Ctrl+Alt+Delete".action.quit = {};
+        "Mod+Shift+E".action.quit = { };
+        "Ctrl+Alt+Delete".action.quit = { };
       };
       environment = {
         "NIXOS_OZONE_WL" = "1";
@@ -249,7 +253,7 @@
     };
   };
 
-  flake.modules.homeManager.niriStandalone = {pkgs, ...}: {
+  flake.modules.homeManager.niriStandalone = { pkgs, ... }: {
     # Standalone niri home-manager module for my non-NixOS systems
     # TODO: On non-NixOS systems I need to figure out graphics...
     # at the moment, I just build niri from source and don't even use this module
@@ -267,10 +271,10 @@
     # TODO: Maybe I should just use the system installed xdg portals on non-nixos systems...
     # because this doesn't seem to work? Although it is also probably because of broken graphics...
     xdg.portal.enable = true;
-    xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gnome];
+    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
     xdg.portal.config = {
       common = {
-        default = ["gnome"];
+        default = [ "gnome" ];
       };
     };
 
